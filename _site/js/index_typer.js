5b60c9143372a2b57c7095dad7c0e57bc4db6ba9
aidenbenner <aiden.benner@gmail.com>
2016-11-04 23:30:19 -0400

    attempt 2

---
+++ _site/js/index_typer.js
@@ -0,0 +1,8 @@
+  $(document).ready(function(){  
+    $(".typed").typed({
+        strings: ["Hello. ", "My name is Aiden. ", "I study Software Engineering. " ],
+        typeSpeed: 100,
+        backDelay: 2500,
+        loop: true
+    });
+});
